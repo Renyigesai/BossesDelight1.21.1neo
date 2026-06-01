a810c8b17ed6e0526cfbf833c604a3c676f0b563
renyigesai <2907654918@qq.com>
2026-06-01 21:01:48 +0800

    铁掌修改测试，现在掉落物可以包括方块NBT（仅方块实体）2026/06/01

--- src/main/java/io/github/reneg/bossesdelight/common/items/GauntletItem.java
+++ src/main/java/io/github/reneg/bossesdelight/common/items/GauntletItem.java
@@ -4,8 +4,12 @@ import io.github.reneg.bossesdelight.common.config.Config;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
+import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -16,6 +20,7 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CakeBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import vectorwing.farmersdelight.common.block.FeastBlock;
@@ -49,19 +54,41 @@ public class GauntletItem extends KnifeItem implements Equipable {
             BlockPos pos = blockHit.getBlockPos();
             Direction dir = blockHit.getDirection();
             BlockState state = level.getBlockState(pos);
-            if(state.is(ModBlocks.CUTTING_BOARD.get()) || ((state.getBlock() instanceof FeastBlock || state.getBlock() instanceof CakeBlock || state.getBlock() instanceof PieBlock) && !Config.COLLECT_FEAST.get())){
+            if(state.getBlock().defaultBlockState().isAir() || state.is(ModBlocks.CUTTING_BOARD.get()) || ((state.getBlock() instanceof FeastBlock || state.getBlock() instanceof CakeBlock || state.getBlock() instanceof PieBlock) && !Config.COLLECT_FEAST.get())){
                 return;
             }
             if(state.getDestroySpeed(level, BlockPos.containing(pos.getX(), pos.getY(), pos.getZ())) >= 0){
-                if (!player.isCreative()){
-//                    stack.hurtAndBreak(1, player, (l) -> l.broadcastBreakEvent(player.getUsedItemHand()));
+                if (!player.getAbilities().instabuild){
+                    stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(InteractionHand.MAIN_HAND));
                 }
-                level.destroyBlock(pos, false, player);
-                Block.popResourceFromFace(level, pos, dir, state.getBlock().getCloneItemStack(level, pos, state));
-                player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
-                livingEntity.releaseUsingItem();
+                ItemStack dropStack = ItemStack.EMPTY;
+                if (state.hasBlockEntity()){
+                    BlockEntity blockEntity = level.getBlockEntity(pos);
+                    if (blockEntity != null){
+                        dropStack = new ItemStack(state.getBlock());
+                        if (!dropStack.isEmpty()){
+                            addCustomNbtData(dropStack, blockEntity, level.registryAccess());
+                            blockEntity.setRemoved();
+                        }
+                    }
+                }else {
+                    dropStack = state.getBlock().getCloneItemStack(level, pos, state);
+                }
+                if (!dropStack.isEmpty()){
+                    Block.popResourceFromFace(level, pos, dir, dropStack);
+                    player.awardStat(Stats.ITEM_USED.get(stack.getItem()));
+                    level.destroyBlock(pos,false);
+                    livingEntity.releaseUsingItem();
+                }
             }
         }
+    }
+
+    private void addCustomNbtData(ItemStack p_263370_, BlockEntity p_263368_, RegistryAccess p_323897_) {
+        CompoundTag compoundtag = p_263368_.saveCustomAndMetadata(p_323897_);
+        p_263368_.removeComponentsFromTag(compoundtag);
+        BlockItem.setBlockEntityData(p_263370_, p_263368_.getType(), compoundtag);
+        p_263370_.applyComponents(p_263368_.collectComponents());
     }
 
     public UseAnim getUseAnimation(ItemStack stack) {
